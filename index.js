b548846fd68b24eed3410b2318673012efbe4247
Shubham Jha <jhashubham059@gmail.com>
2025-03-30 19:07:23 +0530

    connecting database and importing modules

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import pg from 'pg';
+import bcrypt from 'bcrypt';
+import session from 'express-session';
+import env from 'dotenv';
+import passport from 'passport';
+import { Strategy } from 'passport-local';
 
 const app = express();
 const port = 3000;
 
 const db = new pg.Client({
-    
-}) 
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
+}); 
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.render('main.ejs');
-    console.log("demo");
 });
 
 app.get('/login', (req, res) => {

c3af1ac1fdb71162074f43dde5f9f8a1d135339b
Shubham Jha <jhashubham059@gmail.com>
2025-03-30 18:10:56 +0530

    Creating Routes-1

---
+++ index.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('main.ejs');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs');
+});
+
+app.get('/home', (req, res) => {
+    res.render('home.ejs');
+});
+
+app.get('/about-us', (req, res) => {
+    res.render('about.ejs');
+});
+
+app.get('/recipe', (req, res) => {
+    res.render('recipe.ejs');   
+});
+
+app.post('/login', (req, res) => {
+
+});
+
+app.post('/searchByIngrediants', (req, res) => {
+
+})
+
+app.post('/searchByRecipe', (req, res) => {
+
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});

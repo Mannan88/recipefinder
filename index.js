fd864f4fd453a1e17bb3765d7a706e8166a5de67
Shubham Jha <jhashubham059@gmail.com>
2025-03-30 18:29:24 +0530

    adding ejs for login and style

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import pg from 'pg';
 
 const app = express();
 const port = 3000;
+
+const db = new pg.Client({
+    
+}) 
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
